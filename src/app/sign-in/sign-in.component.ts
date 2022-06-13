83f6adc8d33afbd72a6b8c6d1476ae2682bb2e05
Darlene Costa <darl_mel@hotmail.com>
2022-06-13 06:32:12 -0300

    trabalhando signin

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,21 @@
+import { RequestSignIn } from './../resoruces/models/RequestSignIn';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  public requestSignIn!: RequestSignIn;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.requestSignIn = new RequestSignIn();
   }
 
+  public doLogin(): void {
+    console.log(this.requestSignIn);
+  }
 }
