7bee68529c99d99b90b361bd644a970657df6a3b
Darlene Costa <darl_mel@hotmail.com>
2022-07-05 07:17:59 -0300

    topbar

---
+++ src/app/views/components/store-preference-list/store-preference-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-store-preference-list',
+  templateUrl: './store-preference-list.component.html',
+  styleUrls: ['./store-preference-list.component.css'],
+})
+export class StorePreferenceListComponent {
+  @Input() storesPreference: Array<any> = [];
+  @Input() msgError: string = '';
+  alert = {
+    title: 'Lojas não localizadas',
+    subtitle: 'Utilize o botão abaixo para adicionar suas lojas preferidas',
+  };
+}
