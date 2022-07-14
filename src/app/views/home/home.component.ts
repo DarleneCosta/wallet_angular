4c3e5d50d0bade9b742549b90950b4f6833fa06f
Darlene Costa <darl_mel@hotmail.com>
2022-07-14 19:34:20 -0300

    verifica se esta logado

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticateService } from './../../resources/services/authenticate/authenticate.service';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private authService: AuthenticateService,
+    private router: Router,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
+    this.spinner.show();
+    try {
+      if (this.authService.isAuthenticated()) {
+        this.router.navigate(['dashboard']);
+      }
+    } finally {
+      this.spinner.hide();
+    }
   }
-
 }
